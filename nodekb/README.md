45cef7c10324f0497fd047412cfdb1114e0644db
westsideniqa <frankwe@tlu.ee>
2018-01-02 18:55:02 +0200

    Finalizing

---
+++ nodekb/README.md
@@ -0,0 +1,33 @@
+# E-Store
+
+### Technologies
+Node.js
+Express
+Express Messages, Session, Connect Flash and Validation
+MongoDB and Mongoose
+Pug templating
+Passport.js Authentication
+BCrypt Hashing
+
+## Usage
+
+
+
+### Serve
+start mongod & mongo
+
+gitbash - cd /C/where_you_placed_the_folder/nodekb
+
+
+
+```sh
+$ npm start
+```
+
+Open your browser, open url - localhost:3000
+
+## App Info
+
+### Author
+
+Frank Westholm
--- nodekb/app.js
+++ nodekb/app.js
@@ -113,6 +113,9 @@ let users = require('./routes/users');
 app.use('/products', products);
 app.use('/users', users);
 
+
+/* does not work!
+
 //set storage engine
 
 const storage = multer.diskStorage({
@@ -147,8 +150,6 @@ function checkFileType(file, cb){
   }
 }
 
-//pug
-app.set('view engine', 'pug');
 
 //public folder
 app.use(express.static('./public'));
@@ -175,32 +176,8 @@ app.post('/upload', (req, res) =>{
     }
   });
 });
-
-//uploading pictures
-
-
-/*
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
-  // req.file is the `avatar` file 
-  // req.body will hold the text fields, if there were any 
-})
- 
-app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
-  // req.files is array of `photos` files 
-  // req.body will contain the text fields, if there were any 
-})
- 
-const cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
-app.post('/cool-profile', cpUpload, function (req, res, next) {
-  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files 
-  // 
-  // e.g. 
-  //  req.files['avatar'][0] -> File 
-  //  req.files['gallery'] -> Array 
-  // 
-  // req.body will contain the text fields, if there were any 
-})
 */
+
 //start server
 app.listen(3000, function(){
   console.log('Server started on port 3000');
